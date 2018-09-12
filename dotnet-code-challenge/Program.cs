100d485c0f84946c7acadd4f410c9a7072269807
Chris Green <cgreen@anzgcis.com>
2018-09-12 22:34:40 +1000

    Template out the program flow at the highest level

--- dotnet-code-challenge/Program.cs
+++ dotnet-code-challenge/Program.cs
@@ -2,11 +2,20 @@
 
 namespace dotnet_code_challenge
 {
-    class Program
+    public class Program
     {
+        /// <summary>
+        /// A Console App to output horse names from a data feed in ascending order of price.
+        /// </summary>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // create instance of repos
+
+            // retrieve the horses, merge them and order
+
+            // output the horse names in the console
+
+            Console.ReadKey();
         }
     }
 }
