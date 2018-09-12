741e388319be1ae89fbf7f29e3ce1114c21446d3
Chris Green <cgreen@anzgcis.com>
2018-09-13 00:03:10 +1000

    update program main method - push repo call into seperate method.

--- dotnet-code-challenge/Program.cs
+++ dotnet-code-challenge/Program.cs
@@ -1,21 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using dotnet_code_challenge.Model;
 using dotnet_code_challenge.Repository;
 
 namespace dotnet_code_challenge
 {
     public class Program
     {
+        private static readonly WolferhamptonRaceRepository WolferHamptonRepo = new WolferhamptonRaceRepository();
+
         /// <summary>
         /// A Console App to output horse names from a data feed in ascending order of price.
         /// </summary>
         static void Main(string[] args)
         {
-            // create horse race repos
-            var wolferHamptonRepo = new WolferhamptonRaceRepository();
-
             // retrieve the horses, and order them by price ascending
-            var horses = wolferHamptonRepo.GetHorses().OrderBy(h => h.Price);
+            var horses = GetHorsesInOrder();
 
             // output the horse names in the console
             Console.WriteLine("Horses Names in Price ascending Order:\n");
@@ -26,6 +27,11 @@ namespace dotnet_code_challenge
             }
 
             Console.ReadKey();
+        }
+
+        private static IEnumerable<Horse> GetHorsesInOrder()
+        {
+            return WolferHamptonRepo.GetHorses().OrderBy(h => h.Price);
         }
     }
 }
