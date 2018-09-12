9859545e64db2657c7a7d43bea48d39b4c1ba09e
Chris Green <cgreen@anzgcis.com>
2018-09-12 23:47:28 +1000

    add wolferhampton repository test class. add test case to assert two horses returned from repo when using the wolferhampton default file as test json input.

---
+++ dotnet-code-challenge.Test/Repository/WolferhamptonRepositoryTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace dotnet_code_challenge.Test.Repository
+{
+    public class WolferhamptonRepositoryTest : BaseRepositoryTest
+    {
+        [Fact]
+        public void GivenWolferhamptonRepositoryWithTwoHorses_WhenRetrievalOfHorses_ThenShouldGetTwoHorses()
+        {
+            // arrange, act, assert
+            var repo = CreateWolferhamptonRepository();
+            var expectedNumberOfHorses = 2;
+
+            var actualNumberOfHorses = repo.GetHorses().Count();
+
+            Assert.Equal(expectedNumberOfHorses, actualNumberOfHorses);
+        }
+    }
+}
