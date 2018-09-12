a02095865e00a83e42ebca35391e0feba9c239e0
Chris Green <cgreen@anzgcis.com>
2018-09-12 22:30:15 +1000

    add horse model.

---
+++ dotnet-code-challenge/Model/Horse.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_code_challenge.Model
+{
+    public class Horse
+    {
+        public string Id { get; set; }
+        public string Num { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
