0c05b49f9de413d7065817eee96e0df3257a2a25
Chris Green <cgreen@anzgcis.com>
2018-09-12 23:26:14 +1000

    add market selection model. json data loader implementation wip - drill into the data of interest.

--- dotnet-code-challenge/DataAccess/JsonDataLoader.cs
+++ dotnet-code-challenge/DataAccess/JsonDataLoader.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
 using dotnet_code_challenge.Model;
+using Newtonsoft.Json.Linq;
 
 namespace dotnet_code_challenge.DataAccess
 {
@@ -19,7 +20,23 @@ namespace dotnet_code_challenge.DataAccess
             var horses = Enumerable.Empty<Horse>();
             var jsonString = File.ReadAllText(filePath);
             dynamic data = JsonConvert.DeserializeObject<dynamic>(jsonString);
+            var rawData = data["RawData"];
 
+            var participants = rawData["Participants"];
+            if (participants != null)
+            {
+                var markets = rawData["Markets"];
+                var selections = ((JArray)markets).Select(m => m["Selections"]);
+                var selectionItems = selections.SelectMany(si => si);
+                var marketSelections = selectionItems.Select(s => new MarketSelection
+                {
+                    Id = (string)s["Id"],
+                    Price = (decimal)s["Price"],
+                    ParticipantId = (string)s["Tags"]["participant"]
+                }).ToArray();
+
+
+            }
 
             return horses;
         }
---
+++ dotnet-code-challenge/Model/MarketSelection.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_code_challenge.Model
+{
+    public class MarketSelection
+    {
+        public string Id { get; set; }
+        public decimal Price { get; set; }
+        public string ParticipantId { get; set; }
+    }
+}
