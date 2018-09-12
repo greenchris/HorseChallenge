bf4e5a500fd0f56ab3dd44adf83163d92f9dcc04
Chris Green <cgreen@anzgcis.com>
2018-09-12 22:48:32 +1000

    added JsonDataLoader and templated it for implementation.

---
+++ dotnet-code-challenge/DataAccess/JsonDataLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using dotnet_code_challenge.Model;
+
+namespace dotnet_code_challenge.DataAccess
+{
+    public class JsonDataLoader
+    {
+        private static readonly string filePath = @"FeedData\Wolferhampton_Race1.json";
+
+        /// <summary>
+        /// Retrieves Race Horses from a Wolferhampton Race and 
+        /// projects them as Horses
+        /// </summary>
+        public IEnumerable<Horse> LoadWolferHamptonHorses()
+        {
+            var horses = Enumerable.Empty<Horse>();
+
+            // TODO - do stuff!!
+
+            return horses;
+        }
+    }
+}
