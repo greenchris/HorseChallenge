2a1dbc608aa19e266aed6a76d836e67ce2e00d38
Chris Green <cgreen@anzgcis.com>
2018-09-12 23:41:02 +1000

    add base repository test class to serve as object mother

---
+++ dotnet-code-challenge.Test/Repository/BaseRepositoryTest.cs
@@ -0,0 +1,12 @@
+﻿using dotnet_code_challenge.Repository;
+
+namespace dotnet_code_challenge.Test.Repository
+{
+    public class BaseRepositoryTest
+    {
+        protected WolferhamptonRaceRepository CreateWolferhamptonRepository()
+        {
+            return new WolferhamptonRaceRepository();
+        }
+    }
+}
