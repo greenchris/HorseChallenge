c82371e1cb85cd6a9a0de564c132f98724abe713
Chris Green <cgreen@anzgcis.com>
2018-09-12 22:41:07 +1000

    Add Race repository interface and shell of WolferhamptonRaceRepository to build out Wolferhampton functionality.

---
+++ dotnet-code-challenge/Repository/Contracts/IRaceRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using dotnet_code_challenge.Model;
+
+namespace dotnet_code_challenge.Repository.Contracts
+{
+    public interface IRaceRepository
+    {
+        IEnumerable<Horse> GetHorses();
+    }
+}
---
+++ dotnet-code-challenge/Repository/WolferhamptonRaceRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using dotnet_code_challenge.Model;
+using dotnet_code_challenge.Repository.Contracts;
+
+namespace dotnet_code_challenge.Repository
+{
+    /// <summary>
+    /// Repository that abstracts away data access for retrieving Horses from the 
+    /// Wolferhampton race 
+    /// </summary>
+    public class WolferhamptonRaceRepository : IRaceRepository
+    {
+        public IEnumerable<Horse> GetHorses()
+        {
+            // TODO - create json data loader in data access layer
+            throw new NotImplementedException();
+        }
+    }
+}
